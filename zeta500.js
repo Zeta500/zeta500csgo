35c7aa1d1939b8ac6a69a7a8bc1be6ffb4efea97
Zeta500 <45313052+Zeta500@users.noreply.github.com>
2018-11-24 15:19:37 +0100

    Create zeta500.js

---
+++ zeta500.js
@@ -0,0 +1,11 @@
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+
+
+
+money = document.getElementById("balance").innerHTMl;
+console.log(money);
